ebba8617ba6eaa5b49b8170e3c432310a1448d70
leothebeagle <a.kosber@gmail.com>
2019-07-25 19:16:21 -0400

    write migration for teachers

--- db/migrate/20190725222025_create_teachers.rb
+++ db/migrate/20190725222025_create_teachers.rb
@@ -1,7 +1,11 @@
 class CreateTeachers < ActiveRecord::Migration[5.2]
   def change
     create_table :teachers do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :bio
+      t.sttring :password_digest
       t.timestamps
     end
   end
