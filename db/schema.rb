9ae6a3352ca2bb482a59a7bd636c209ceb70e0c6
leothebeagle <a.kosber@gmail.com>
2019-07-25 19:19:27 -0400

    include teachers table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_214657) do
+ActiveRecord::Schema.define(version: 2019_07_25_222025) do
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "bio"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
