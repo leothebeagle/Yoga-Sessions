5c40d0bf5514701dc3d59d12161c06a82066acd5
leothebeagle <a.kosber@gmail.com>
2019-07-26 22:25:38 -0400

    Correct form for new teacher and add views
    Remakes the teacher form. Previously had used
    rails generate controller action action, to make the controller.
    This was yielding an error, because the @teacher instance variable in
    teachers#new was not passing on to the new.html.erb view file.
    
    When redone using rails generate controller without specifying
    controller actions it works. Manually constructed contents
    of controller as well as created the view files.
    
    This is a working flow for a new teacher to sign up,
    without authentication.

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,18 +1,25 @@
 class TeachersController < ApplicationController
-  def show
-  end
+    
+    def show
+        @teacher = Teacher.find(params[:id])
+    end
+    
+    def new
+       @teacher = Teacher.new
+    end
 
-  def new
-    @teacher = Teacher.new
-  end
+    def create
+        teacher = Teacher.new(teacher_params)
+        
+        if teacher.save
+            redirect_to teacher_path(teacher)
+        else
+            render 'new'
+        end
+    end
 
-  def create
-    raise params.inspect
-  end
+    def teacher_params
+        params.require(:teacher).permit(:first_name, :last_name, :email, :bio, :password, :password_confirmation)
+    end
 
-  private
-
-  def teacher_params
-    params.require(:teacher).permit(:first_name, :last_name, :email, :bio, :password, :password_confirmation)
-  end
 end
