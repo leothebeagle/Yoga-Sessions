94a8eeb078f4ee1c98745b2be0cdd43977c613ef
leothebeagle <a.kosber@gmail.com>
2019-07-29 16:18:36 -0400

    Add routes for logging in and view

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
 
   resources :programs, only: [:show, :new, :create]
 
+    get '/teacher/login' => "sessions#new"
+    post '/teacher/login' => "sessions#create"
+    get '/login' => "sessions#new"
+    post '/login' => "sessions#create"
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
