77e07cecddf896f03d3bf682f89b9aaba3fcfbe5
leothebeagle <a.kosber@gmail.com>
2019-07-29 17:17:00 -0400

    Add checkbox to identify teacher logging in
    Adds a checkbox in sessions/new to indicate that a teacher is
    logging in. Also writes create action in SessionsController
    to distinguish between logging a studetn in vs a teacher

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,29 @@
 class SessionsController < ApplicationController
 
     def new 
-        raise params.inspect
+        
     end
 
     def create 
-
+        if params[:teacher][:login] == "0"
+            @student = Student.find_by(email: params[:email])
+            if @student.authenticate(params[:password])
+                session[:student_id] = @student.id
+                redirect_to student_path @student
+            else
+                render login_path
+            end
+        elsif params[:teacher][:login] == "1"
+            @teacher = Teacher.find_by(email: params[:email])
+            if @teacher.authenticate(params[:password])
+                redirect_to teacher_path @teacher 
+            else 
+                render login_path @teacher 
+            end
+        end
     end
+
+    private 
+
+
 end
