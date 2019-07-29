db6cd1d8b6aa89c3ab9a58f8141f2055173ac1e5
leothebeagle <a.kosber@gmail.com>
2019-07-29 17:30:16 -0400

    Add logout feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
 
     def create 
         if params[:teacher][:login] == "0"
+            puts "logging in as student"
             @student = Student.find_by(email: params[:email])
             if @student.authenticate(params[:password])
                 session[:student_id] = @student.id
@@ -14,6 +15,7 @@ class SessionsController < ApplicationController
                 render login_path
             end
         elsif params[:teacher][:login] == "1"
+            puts "logging in as teacher"
             @teacher = Teacher.find_by(email: params[:email])
             if @teacher.authenticate(params[:password])
                 redirect_to teacher_path @teacher 
@@ -21,6 +23,11 @@ class SessionsController < ApplicationController
                 render login_path @teacher 
             end
         end
+    end
+
+    def destroy
+        session.clear
+        redirect_to login_path
     end
 
     private 
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   
     get '/login' => "sessions#new"
     post '/login' => "sessions#create"
+    get '/logout' => "sessions#destroy"
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
