e5f166dc97b70aa68ce7606570109d9bda810ebb
leothebeagle <a.kosber@gmail.com>
2019-07-28 21:24:05 -0400

    Add ability to create a new program
    This is not the final version yet. This gives teacher the ability to
    create new programs and add videos. But the program is not linked
    to a teacher.

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,2 +1,27 @@
 class ProgramsController < ApplicationController
+
+    def show
+        @program = Program.find_by(id: params[:id])
+    end
+
+    def new
+        @program = Program.new 
+        @videos = Teacher.first.videos
+    end
+
+    def create
+        @program = Program.new(program_params)
+
+        if program.save
+            redirect_to program_path(@program)
+        else
+            render new_program_path
+        end
+    end
+
+    private 
+
+    def program_params
+        params.require(:program).permit(:name, :description, :teacher_id, :video_ids => [])
+    end
 end
