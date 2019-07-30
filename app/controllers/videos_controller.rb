13293289358d21d47385d6eef29e19f6c2449e58
leothebeagle <a.kosber@gmail.com>
2019-07-30 17:05:20 -0400

    Add controls for VideosController actions
    Only allow logged in teachers to access new and create actions for
    videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,8 @@
 class VideosController < ApplicationController
    #everything here will require a login. create action will depend on the teacher creating it.  
+   before_action :require_teacher_login
+   skip_before_action :require_teacher_login, only: [:show]
+
     def show
         @video = Video.find_by(id: params[:id])
     end
@@ -24,5 +27,9 @@ class VideosController < ApplicationController
     def video_params
         params.require(:video).permit(:title, :description, :duration, :teacher_id)
     end
+
+    def require_teacher_login
+        return head(:forbidden) unless session.include? :teacher_id
+    end 
 
 end
