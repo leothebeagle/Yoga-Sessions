58c1f6e91cc06802bb0904b417e15f073437c2c8
leothebeagle <a.kosber@gmail.com>
2019-07-29 22:26:41 -0400

    Add controls for ProgramsController actions
    Require user to be logged in to access show and index.
    
    Require user to be logged in as a teacher to access new and create.

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,5 +1,9 @@
 class ProgramsController < ApplicationController
+    before_action :require_login, only: [:index, :show]
 
+    before_action :require_teacher_login
+    skip_before_action :require_teacher_login, only: [:index, :show]
+    
     def index
         @programs = Program.all 
     end
@@ -10,14 +14,16 @@ class ProgramsController < ApplicationController
 
     def new
         @program = Program.new 
-        @videos = Teacher.first.videos
+        @videos = Video.all 
     end
 
     def create
+    
         @program = Program.new(program_params)
+        binding.pry
 
-        if program.save
-            redirect_to program_path(@program)
+        if @program.save
+            redirect_to program_path(@program) 
         else
             render new_program_path
         end
@@ -26,6 +32,21 @@ class ProgramsController < ApplicationController
     private 
 
     def program_params
-        params.require(:program).permit(:name, :description, :teacher_id, :video_ids => [])
+        params.require(:program).permit(:name, :description, :teacher_id, video_ids: [])
     end
+
+    def require_teacher_login
+        return head(:forbidden) unless session.include? :teacher_id
+    end
+
+    def require_login 
+        if session.include?(:teacher_id) || session.include?(:student_id) 
+            true 
+        else
+            return head(:forbidden)
+        end
+    end
+
+   
+
 end
