b96d41363a51c543000ad407966e5c892c902f02
leothebeagle <a.kosber@gmail.com>
2019-07-29 22:22:10 -0400

    Add methods to application controller
    This add methods to application controller to determine who the current
    user is.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    def teacher? 
+        !!session[:teacher_id]
+    end
+
+    def student? 
+        !!student[:student_id]
+    end
+
+    def current_teacher
+        @current_teacher ||= @current_teacher = Teacher.find_by(id: session[:teacher_id])
+    end
+
+    def current_student
+        @current_student ||= @current_student = Student.find_by(id: session[:student_id])
+    end
 end
