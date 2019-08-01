8380beb8d764e9d82f8fdaf9cf50e70fde68f45f
leothebeagle <a.kosber@gmail.com>
2019-08-01 12:09:31 -0400

    Change redirect for adding  new program to library

--- app/controllers/library_items_controller.rb
+++ app/controllers/library_items_controller.rb
@@ -5,7 +5,7 @@ class LibraryItemsController < ApplicationController
             current_student.programs << program
             current_student.save
             flash[:notice] = "You successfully added this program to your library!!!!"
-            redirect_to student_path @current_student
+            redirect_to student_programs_path @current_student
         else
             flash[:notice] = "You already have this program in your library"
             redirect_to student_programs_path @current_student 
