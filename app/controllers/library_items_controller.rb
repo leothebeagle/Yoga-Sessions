477fe4d5e75ccf3cd4b1b1edff91866075c23db8
leothebeagle <a.kosber@gmail.com>
2019-08-01 11:57:07 -0400

    Build create action in LibraryItems controller
    Builds create action to include ability to tell if a student has already
    included a program in their library.
    
    Also, adds flash messages for successful addition of a program and
    appropriate redirects

--- app/controllers/library_items_controller.rb
+++ app/controllers/library_items_controller.rb
@@ -1,10 +1,15 @@
 class LibraryItemsController < ApplicationController
     def create 
-        
         program = Program.find_by(id: params[:library_item][:program_id])
-        current_student.programs << program
-        current_student.save
-        redirect_to student_path @current_student
+        if !current_student.programs.include?(program)
+            current_student.programs << program
+            current_student.save
+            flash[:notice] = "You successfully added this program to your library!!!!"
+            redirect_to student_path @current_student
+        else
+            flash[:notice] = "You already have this program in your library"
+            redirect_to student_programs_path @current_student 
+        end
     end 
 end
 
