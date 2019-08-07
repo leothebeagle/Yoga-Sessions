f30d073249c522d8aacfe6ae4571243e6b7900b5
leothebeagle <a.kosber@gmail.com>
2019-08-06 22:26:01 -0400

    Add callback route for ig login with oauth

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
     get '/login' => "sessions#new"
     post '/login' => "sessions#create"
     get '/logout' => "sessions#destroy"
+
+    get '/auth/instagram/callback' => "sessions#create"
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
