c88569eec4169288d643f533d74359b97774f794
leothebeagle <a.kosber@gmail.com>
2019-08-06 22:25:12 -0400

    Modify create action in sessions controller
    This modifies the create action in the sessions controller to account
    for instagram login with omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,18 @@ class SessionsController < ApplicationController
         
     end
 
-    def create 
-        if params[:teacher][:login] == "0"
+    def create
+        if params.include?("instagram")
+            @student = Student.find_or_create_by(uid: auth['uid']) do |u|
+                u.first_name = auth['info']['name']
+                u.email = auth['info']['email']
+                u.image = auth['info']['image']
+            end
+
+            session[:student_id] = @student.id 
+            redirect_to student_path @student
+
+        elsif params[:teacher][:login] == "0"
             @student = Student.find_by(email: params[:email])
             if @student.authenticate(params[:password])
                 session[:student_id] = @student.id
@@ -27,6 +37,12 @@ class SessionsController < ApplicationController
     def destroy
         session.clear
         redirect_to login_path
+    end
+
+    private 
+
+    def auth
+        request.env['omniauth.auth']
     end
 
 
