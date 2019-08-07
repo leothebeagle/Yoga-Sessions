61783d47589746090ed823ee5060d152688a4bbd
leothebeagle <a.kosber@gmail.com>
2019-08-07 19:11:58 -0400

    Add back validations to Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,14 +1,14 @@
 class Student < ApplicationRecord
-    # has_secure_password
+    has_secure_password
 
-    # validates :first_name, presence: true
+    validates :first_name, presence: true
     
-    # validates :last_name, presence: true
+    validates :last_name, presence: true
     
-    # validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
 
-    # validates :password, presence: true
-    # validates :password_confirmation, presence: true
+    validates :password, presence: true
+    validates :password_confirmation, presence: true
     
     has_many :library_items
     has_many :programs, through: :library_items 
