dd6bb6694cbe848a949ac451447d4c0bba8da9fc
leothebeagle <a.kosber@gmail.com>
2019-08-08 13:45:04 -0400

    Add strong params in application controller
    Add to devise's default permitted paramters in student sign up.
    
    For this app's purposes, would like to have a first name and
    last name for students

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
     # def teacher? 
     #     !!session[:teacher_id]
     # end
@@ -14,4 +15,11 @@ class ApplicationController < ActionController::Base
     # def current_student
     #     @current_student ||= @current_student = Student.find_by(id: session[:student_id])
     # end
+
+    protected 
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    end
+
 end
