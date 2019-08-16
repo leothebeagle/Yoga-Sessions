c7aea7800298497cc6a395f9af079979358f5017
leothebeagle <a.kosber@gmail.com>
2019-08-16 14:39:53 -0400

    Write partial and actions for favoriting links

--- app/controllers/library_items_controller.rb
+++ app/controllers/library_items_controller.rb
@@ -1,12 +1,12 @@
 class LibraryItemsController < ApplicationController
     def create 
+
         program = Program.find_by(id: params[:program_id])
         
         # refactor to a method that can be called like this: current_student.add_to_library(program)
          
-        if !current_student.programs.include?(program)
-            current_student.programs << program
-            current_student.save
+        if !current_student.has_program?(program)
+            current_student.add_program_to_library(program)
             flash[:notice] = "You successfully added this program to your library!!!!"
             redirect_to student_programs_path(current_student)
         else
@@ -14,6 +14,12 @@ class LibraryItemsController < ApplicationController
             redirect_to student_programs_path(current_student) 
         end
     end 
+
+    def destroy
+        item_to_delete = LibraryItem.find_by(program_id: params[:program_id], student_id: current_student.id).destroy
+        flash[:notice] = "You deleted a program from your library"
+        redirect_to student_programs_path(current_student)
+    end
 end
 
 
