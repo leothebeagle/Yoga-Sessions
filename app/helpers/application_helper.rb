089ab71864794d0882d7ea2b33142b4a8bbda586
leothebeagle <a.kosber@gmail.com>
2019-08-22 17:11:38 -0400

    Write helper method for navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
     def navbar
-        if current_page?("/")
+        if current_page?("/") && !current_student
             render partial: "layouts/nav_student_logged_out"
-        elsif current_page?("/teachers/homepage")
+        elsif current_page?("/teachers/homepage") && !current_teacher
             render partial: "layouts/nav_teacher_logged_out"
+        elsif current_student 
+            render partial: "layouts/nav_student_logged_in"
+        elsif current_teacher 
+            render partial: "layouts/nav_teacher_logged_in"
         end
     end
 end
