ecc40cc7d6e9157452d0ffc2860fb3adc37015a8
leothebeagle <a.kosber@gmail.com>
2019-08-22 12:32:47 -0400

    Write a helper method to render navbar partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def navbar
+        if current_page?("/")
+            render partial: "layouts/nav_student_logged_out"
+        elsif current_page?("/teachers/homepage")
+            render partial: "layouts/nav_teacher_logged_out"
+        end
+    end
 end
